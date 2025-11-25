ec1f100dcec080bffdc8380e0646f578189f6b2f
Alex Bergdahl <bergdahlalex@protonmail.com>
2025-11-25 15:57:58 +0100

    set up hlchunk

---
+++ lua/custom/plugins/hlchunk.lua
@@ -0,0 +1,39 @@
+return {
+  'shellRaining/hlchunk.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local kanagawa_colors = require('kanagawa.colors').setup({ theme = 'wave' })
+    if not kanagawa_colors then
+      return
+    end
+
+    require('hlchunk').setup({
+      chunk = {
+        enable = true,
+        use_treesitter = true,
+        chars = {
+          horizontal_line = '─',
+          vertical_line = '│',
+          left_top = '┌',
+          left_bottom = '└',
+          right_arrow = '─',
+        },
+        delay = 0,
+        duration = 100,
+        style = {
+          kanagawa_colors.palette.roninYellow, -- Primary chunk color
+          kanagawa_colors.palette.autumnRed, -- Error-containing chunks
+        },
+      },
+      indent = {
+        enable = true,
+        chars = {
+          '│',
+        },
+        style = {
+          kanagawa_colors.palette.sumiInk5, -- Subtle indent guide color
+        },
+      },
+    })
+  end,
+}
